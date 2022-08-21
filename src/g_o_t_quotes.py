f41c03f2112b0c032eb6d4b7fb29bc51db1af2b8
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2022-08-22 00:59:20 +0600

    Create g_o_t_quotes.py

---
+++ src/g_o_t_quotes.py
@@ -0,0 +1,43 @@
+from requests import get
+
+
+class GameOfThronesQuotes:
+	def __init__(self):
+		self.api = "https://api.gameofthronesquotes.xyz/v1"
+		self.headers = {
+			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",
+			"accept": "application/json"
+		}
+	
+	def get_random_quote(self, number: int = 1):
+		return get(
+			f"{self.api}/random/{number}",
+			headers=self.headers).json()
+	
+	def get_character_quotes(
+			self,
+			character_name: str,
+			number: int = 1):
+		return get(
+			f"{self.api}/author/{character_name}/{number}",
+			headers=self.headers).json()
+	
+	def get_houses_list(self):
+		return get(
+			f"{self.api}/houses",
+			headers=self.headers).json()
+	
+	def get_house_by_name(self, house_name: str):
+		return get(
+			f"{self.api}/house/{house_name}",
+			headers=self.headers).json()
+	
+	def get_characters_list(self):
+		return get(
+			f"{self.api}/characters",
+			headers=self.headers).json()
+	
+	def get_character_details(self, character_name: str):
+		return get(
+			f"{self.api}/characters/{character_name}",
+			headers=self.headers).json()
