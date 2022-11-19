d2abdc1bf25308d9cd14ae401e246cf8567a1169
ecclesias <77536370+zeviel@users.noreply.github.com>
2022-11-19 20:35:02 +0800

    Update g_o_t_quotes.py

--- src/g_o_t_quotes.py
+++ src/g_o_t_quotes.py
@@ -1,15 +1,14 @@
 from requests import get
 
-
 class GameOfThronesQuotes:
-	def __init__(self):
+	def __init__(self) -> None:
 		self.api = "https://api.gameofthronesquotes.xyz/v1"
 		self.headers = {
 			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",
 			"accept": "application/json"
 		}
 	
-	def get_random_quote(self, number: int = 1):
+	def get_random_quote(self, number: int = 1) -> dict:
 		return get(
 			f"{self.api}/random/{number}",
 			headers=self.headers).json()
@@ -17,27 +16,27 @@ class GameOfThronesQuotes:
 	def get_character_quotes(
 			self,
 			character_name: str,
-			number: int = 1):
+			number: int = 1) -> dict:
 		return get(
 			f"{self.api}/author/{character_name}/{number}",
 			headers=self.headers).json()
 	
-	def get_houses_list(self):
+	def get_houses_list(self) -> dict:
 		return get(
 			f"{self.api}/houses",
 			headers=self.headers).json()
 	
-	def get_house_by_name(self, house_name: str):
+	def get_house_by_name(self, house_name: str) -> dict:
 		return get(
 			f"{self.api}/house/{house_name}",
 			headers=self.headers).json()
 	
-	def get_characters_list(self):
+	def get_characters_list(self) -> dict:
 		return get(
 			f"{self.api}/characters",
 			headers=self.headers).json()
 	
-	def get_character_details(self, character_name: str):
+	def get_character_details(self, character_name: str) -> dict:
 		return get(
 			f"{self.api}/characters/{character_name}",
 			headers=self.headers).json()
